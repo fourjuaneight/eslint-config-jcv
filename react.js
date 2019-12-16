7f193fce4d35092dc83b77c39523426587546925
Juan Villela <juan@villela.co>
2019-12-16 01:35:36 -0500

    Added React rules and plugins.

---
+++ react.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: ['prettier/react'],
+  plugins: ['react-hooks'],
+  rules: {
+    'jsx-a11y/accessible-emoji': 0,
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        aspects: ['invalidHref'],
+      },
+    ],
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }],
+    'jsx-a11y/label-has-for': 0,
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/display-name': 1,
+    'react/forbid-prop-types': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'react/no-array-index-key': 0,
+    'react/no-unescaped-entities': 0,
+    'react/prefer-stateless-function': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/require-default-props': 0,
+  },
+};
